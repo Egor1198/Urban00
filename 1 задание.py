25b9a99321a6856ce465cdfaa91c867bf6ab676b
Egor1198 <belosokhovegor@gmail.com>
2024-07-29 16:16:00 +0300

    Add files via upload

---
+++ 1 задание.py
@@ -0,0 +1,12 @@
+#1st program
+print(9**0.5*5)
+
+#2nd program
+print(9.99>9.98 and 1000!=1001)
+
+#3nd program
+print(int(1234//10%100)+int(5678//10%100))
+
+
+#4th program
+print(int(13.42)==(42.13*100%4000%200) or int(42.13)==(13.42*100%1000%100))
